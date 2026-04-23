2dc3e011ae2f4bdcc4ee5ed51bde76f7c0f2cd6d
Nadiaaaaa04 <oktavianadiasalwa@gmail.com>
2026-04-23 15:06:11 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleForm() {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
+    } else {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+    }
+}
